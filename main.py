7c66d7f20ac3ce3df6c747ad0bc4ee2ed7c0542a
SHamil36 <seth.hamilton@aol.com>
2022-02-09 23:42:03 -0700

    login page that will connect to mysql database

---
+++ main.py
@@ -0,0 +1,106 @@
+import logging
+import sys
+import os
+import time
+from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QLabel, QGridLayout, QSizePolicy
+from PyQt6.QtCore import Qt
+from PyQt6.QtSql import QSqlDatabase, QSqlQuery
+import mysql.connector
+
+
+class MainApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(800, 600)
+        label = QLabel('Main App', parent=self)
+
+class LoginWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Login Window')
+        self.window_width, self.window_height = 600, 200
+        self.setFixedSize(self.window_width, self.window_height)
+
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        labels = {}
+        self.lineEdits = {}
+
+        labels['Username'] = QLabel('Username')
+        labels['Password'] = QLabel('Password')
+        labels['Username'].setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        labels['Password'].setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+
+        self.lineEdits['Username'] = QLineEdit()
+        self.lineEdits['Password'] = QLineEdit()
+        self.lineEdits['Password'].setEchoMode(QLineEdit.EchoMode.Password)
+
+        layout.addWidget(labels['Username'],            0,0,1,1)
+        layout.addWidget(self.lineEdits['Username'],    0,1,1,3)
+
+        layout.addWidget(labels['Password'],            1,0,1,1)
+        layout.addWidget(self.lineEdits['Password'],    1,1,1,3)
+
+        button_login = QPushButton('&Log In', clicked=self.checkCredential)
+        layout.addWidget(button_login,                  2,4,1,1)
+
+        self.status = QLabel('')
+        self.status.setStyleSheet('font-size: 25px; color: red;')
+        layout.addWidget(self.status, 3,0,1,3)
+
+        self.connectToDB()
+
+    def connectToDB(self):
+        db = QSqlDatabase.addDatabase('QMYSQL')
+        db.setHostName('localhost')
+        db.setDatabaseName('project')
+        db.setUserName('Seth')
+        db.setPassword('JuliaOmen0404!!')
+        
+        if not db.open():
+            self.status.setText('Connection failed')
+
+    def checkCredential(self):
+        username = self.lineEdits['Username'].text()
+        password = self.lineEdits['Password'].text()
+
+        query = QSqlQuery()
+        query.prepare('SELECT * FROM username WHERE username=:username')
+        query.bindValue(':username', username)
+        query.exec()
+
+        if query.first():
+            if query.value('Password') == password:
+                time.sleep(1)
+                self.mainApp = MainApp()
+                self.mainApp.show()
+                self.close()
+            else:
+                self.status.setTExt('Password is incorrect')
+
+        else:
+            self.status.setTExt('Username is not found')
+
+
+
+if __name__ == '__main__':
+
+
+    app = QApplication(sys.argv)
+    app.setStyleSheet('''
+        QWidget {
+            font-size: 25px;
+        }
+        QLineEdit {
+            height: 200px;
+        }
+    ''')
+
+    loginWindow = LoginWindow()
+    loginWindow.show()
+
+    try:
+        sys.exit(app.exec())
+    except SystemExit:
+        print('Closing Window...')
